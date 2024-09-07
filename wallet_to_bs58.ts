9ee0647269d00e27dff2be212e589749d45811a7
Carl Joseph Bugaoan <cjtb2005@gmail.com>
2024-09-07 10:08:34 +0800

    Update

--- wallet_to_bs58.ts
+++ wallet_to_bs58.ts
@@ -1,12 +1,15 @@
 import bs58 from "bs58";
+
 (async () => {
-  const PRIVATE_KEY =
-    "4Ey5v1YsT7zUa5hBzc6G1nTr8BTAfoPkm6GSdVPAW9hVXPLigP73tTSFS8bka692CCM5VZRqjHwrhFkfyH1K3zqP";
+  const PRIVATE_KEY = "4Ey5v1YsT7zUa5hBzc6G1nTr8BTAfoPkm6GSdVPAW9hVXPLigP73tTSFS8bka692CCM5VZRqjHwrhFkfyH1K3zqP";
+
   const decoded = bs58.decode(PRIVATE_KEY);
 
   console.log(decoded);
 
   const encoded = bs58.encode(decoded);
-
+  
   console.log(encoded);
+  
 })();
+
