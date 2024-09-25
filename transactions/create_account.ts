dc66f8e66d25be2fc5b52d1850211ee6bdc5d0f1
Carl Joseph Bugaoan <cjtb2005@gmail.com>
2024-09-25 19:35:33 +0800

    Update

--- transactions/create_account.ts
+++ transactions/create_account.ts
@@ -1,43 +1,36 @@
-import { clusterApiUrl, Connection, Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { clusterApiUrl, Connection, Keypair, SystemProgram, Transaction } from '@solana/web3.js';
 import bs58 from "bs58";
 
 (async () => {
-    const CONNECTION = new Connection(clusterApiUrl("devnet"));
-    const { lastValidBlockHeight, blockhash} = await CONNECTION.getLatestBlockhash();
-
-
     const PRIVATE_KEY = "4Ey5v1YsT7zUa5hBzc6G1nTr8BTAfoPkm6GSdVPAW9hVXPLigP73tTSFS8bka692CCM5VZRqjHwrhFkfyH1K3zqP";
     const decoded = bs58.decode(PRIVATE_KEY);
 
-    const MY_KEYPAIR = Keypair.fromSecretKey(decoded);
-    const NEW_ACCOUNT_KEYPAIR = Keypair.generate();
+    const CONNECTION = new Connection(clusterApiUrl("devnet"));
+    const { lastValidBlockHeight, blockhash } = await CONNECTION.getLatestBlockhash();
 
-    const tx = new Transaction();  
+    const MY_KEYPAIR = Keypair.fromSecretKey(decoded);
+    const NEW_KEYPAIR = Keypair.generate();
+
     const minimumRent = await CONNECTION.getMinimumBalanceForRentExemption(0);
+    const tx = new Transaction();
 
     tx.instructions = [
         SystemProgram.createAccount({
             fromPubkey: MY_KEYPAIR.publicKey,
-            newAccountPubkey: NEW_ACCOUNT_KEYPAIR.publicKey,
+            newAccountPubkey: NEW_KEYPAIR.publicKey,
             lamports: minimumRent,
             space: 0,
             programId: SystemProgram.programId,
         }),
     ];
-    
-    tx.feePayer = MY_KEYPAIR.publicKey;
-    tx.recentBlockhash = blockhash;
-    tx.lastValidBlockHeight = lastValidBlockHeight;
-    tx.sign(MY_KEYPAIR, NEW_ACCOUNT_KEYPAIR);
 
+    tx.lastValidBlockHeight = lastValidBlockHeight;
+    tx.recentBlockhash = blockhash;
+    tx.sign(MY_KEYPAIR, NEW_KEYPAIR);
 
     const serialized = tx.serialize();
     const txHash = await CONNECTION.sendRawTransaction(serialized);
-    // CONNECTION.sendRawTransaction;
-    // const txHash = await CONNECTION.sendTransaction(tx, [
-    //     MY_KEYPAIR, NEW_ACCOUNT_KEYPAIR,
-    // ]);
 
-    console.log(txHash);
-    }
-)();
+    console.log(txHash);    
+})
+();
--- transactions/transfer_sol.ts
+++ transactions/transfer_sol.ts
@@ -1,39 +1,42 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
-import bs58 from "bs58";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemInstruction, SystemProgram, Transaction } from "@solana/web3.js";
+import bs58 from "bs58"
 
 (async () => {
-    const PRIVATE_KEY = "4Ey5v1YsT7zUa5hBzc6G1nTr8BTAfoPkm6GSdVPAW9hVXPLigP73tTSFS8bka692CCM5VZRqjHwrhFkfyH1K3zqP"
-    const CONNECTION = new Connection(clusterApiUrl("devnet"));
-
+    const PRIVATE_KEY = "fQaqSvseo13Qjaxop5mYikT6ZFenRpxPmR7oUGLFd5hR56kyw4mvxxQ1x59ycTqKN4uQJYBVyc4QxGroPrqk8mQ";
     const decoded = bs58.decode(PRIVATE_KEY);
 
-    const NEW_ACCOUNT_KEYPAIR =  Keypair.generate();
     const MY_KEYPAIR = Keypair.fromSecretKey(decoded);
+    const CONNECTION = new Connection(clusterApiUrl("devnet"));
 
+    const { lastValidBlockHeight, blockhash} = await CONNECTION.getLatestBlockhash();
     const tx = new Transaction();
-    const { lastValidBlockHeight, blockhash } =
-    await CONNECTION.getLatestBlockhash();
 
-    const minimumRent = await CONNECTION.getMinimumBalanceForRentExemption(0);
+    const minRent = await CONNECTION.getMinimumBalanceForRentExemption(0);
 
     tx.instructions = [
         SystemProgram.transfer({
             fromPubkey: MY_KEYPAIR.publicKey,
-            toPubkey: new PublicKey("37u1n4N4ie8bdeDMYJTx9vLg25qbfXtoHP37rSbuRRC6"),
-            lamports: minimumRent,
-            programId: SystemProgram.programId,
+            toPubkey: new PublicKey("7AiCz3SEtk45dFQxzziFUS3SJEQFtNB4TzuG4QzjqXAh"),
+            lamports: minRent,
         }),
     ];
 
     tx.lastValidBlockHeight = lastValidBlockHeight;
     tx.recentBlockhash = blockhash;
-    tx.feePayer = MY_KEYPAIR.publicKey;
+
     tx.sign(MY_KEYPAIR);
 
     const serialized = tx.serialize();
     const txHash = await CONNECTION.sendRawTransaction(serialized);
+
     console.log(txHash);
+})();
 
 
-}
-)();
+
+
+
+
+
+
+
